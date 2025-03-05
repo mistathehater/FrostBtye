516edf3d3e203a3693246e82a503f28420da7046
mistathehater <thedbho@gmail.com>
2025-03-04 21:00:21 -0800

    Update Initiate.lua

--- Initiate.lua
+++ Initiate.lua
@@ -17,7 +17,7 @@ getgenv().PlaceName = PlaceName
 PlaceName = PlaceName:gsub("%b[]", "")
 PlaceName = PlaceName:gsub("[^%a]", "")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/alyssagithub/Scripts/refs/heads/main/FrostByte/Analytics.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mistathehater/FrostBtye/refs/heads/main/Analytics.lua"))()
 
 local Success, Code: string = pcall(game.HttpGet, game, `https://raw.githubusercontent.com/mistathehater/refs/heads/main/{PlaceName}.lua`)
 
